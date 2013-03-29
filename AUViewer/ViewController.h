18c07ad19fac407bc4780e1535eb4e2a269e5c7e
Bryan Tung <bryantung89@gmail.com>
2013-03-29 12:59:22 +0800

    Stream audio using AVPlayer

--- AUViewer/ViewController.h
+++ AUViewer/ViewController.h
@@ -10,13 +10,21 @@
 #import <AVFoundation/AVFoundation.h>
 #import <MediaPlayer/MediaPlayer.h>
 
-@interface ViewController : UIViewController<MPMediaPickerControllerDelegate, UIAlertViewDelegate>{
+@interface ViewController : UIViewController<MPMediaPickerControllerDelegate, UIAlertViewDelegate, AVAudioPlayerDelegate>{
     
 }
 
-@property (weak, nonatomic) IBOutlet UIButton *pickSongButton;
-@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *spinner;
+@property (weak, nonatomic) IBOutlet    UIButton                *pickSongButton;
+@property (weak, nonatomic) IBOutlet    UIActivityIndicatorView *spinner;
+@property (weak, nonatomic) IBOutlet    UISegmentedControl      *streamSelect;
+@property (weak, nonatomic) IBOutlet    UIButton                *playbackControl;
+@property (weak, nonatomic) IBOutlet    UILabel                 *durationLabel;
+@property (weak, nonatomic) IBOutlet    UILabel                 *timingLabel;
+@property (weak, nonatomic) IBOutlet    UIProgressView          *bufferBar;
+@property (weak, nonatomic) IBOutlet    UISlider                *playbackProgress;
 
 - (IBAction)pickASong:(id)sender;
+- (IBAction)streamSeleted:(UISegmentedControl *)sender forEvent:(UIEvent *)event;
+- (IBAction)playbackTap:(UIButton *)sender;
 
 @end
--- AUViewer/ViewController.m
+++ AUViewer/ViewController.m
@@ -5,25 +5,39 @@
 //  Created by Bryan Tung on 3/27/13.
 //  Copyright (c) 2013 Bryan Tung. All rights reserved.
 //
+//  3/27
+//  AVAsset/AVAssetReader/AVAssetWritter/MPMediaPicker
+//  iPod library audio raw export
+//
+//  3/28
+//  AVPlayer
+//  HTTP audio stream request
+//
 
 #import "ViewController.h"
 
 @interface ViewController ()
 
 @property (nonatomic)   MPMediaPickerController *mPicker;
+@property (nonatomic)   AVPlayer                *streamPlayer;
+@property (nonatomic)   NSTimer                 *checkBufferedTimer;
+@property (nonatomic)   NSTimer                 *playbackTimeUpdate;
 
 @end
 
 @implementation ViewController
 
+static NSString *streamPlayerPlaybackNotification = @"streamPlayerPlaybackNotification";
+static NSString *streamPlayerStatusNotification = @"streamPlayerStatusNotification";
+
 #pragma mark -
-#pragma mark MediaPicker
+#pragma mark Media Picker
 
 - (IBAction)pickASong:(id)sender
 {
-    //trigger modal view MPMediaPickerController
+    // trigger modal view MPMediaPickerController
     if (_mPicker) {
-        //dismiss if presented
+        // dismiss if presented
         [_mPicker dismissViewControllerAnimated:YES completion:nil];
         _mPicker = nil;
         return;
@@ -43,14 +57,14 @@
 
 - (void)mediaPicker:(MPMediaPickerController *)mediaPicker didPickMediaItems:(MPMediaItemCollection *)mediaItemCollection
 {
-    //user has picked an item
+    // user has picked an item
     [mediaPicker dismissViewControllerAnimated:YES completion:nil];
     MPMediaItem *pickedItem = [[mediaItemCollection items] objectAtIndex:0];
-    //put UI to ignore state
+    // put UI to ignore state
     [[UIApplication sharedApplication] beginIgnoringInteractionEvents];
     [_spinner startAnimating];
-    //call export operation
-    //return if operation has failed
+    // call export operation
+    // return if operation has failed
     if (![self readAudioChunksWithMediaItem:pickedItem]) {
         [[UIApplication sharedApplication] endIgnoringInteractionEvents];
         [_spinner stopAnimating];
@@ -64,11 +78,11 @@
 
 - (BOOL)readAudioChunksWithMediaItem:(MPMediaItem *)mediaItem
 {
-    //using AVFoundation AVAsset API to read and write (AVAssetReader/AVAssetWriter)
-    //get picked file from picker
+    // using AVFoundation AVAsset API to read and write (AVAssetReader/AVAssetWriter)
+    // get picked file from picker
     AVURLAsset *asset = [AVURLAsset assetWithURL:[mediaItem valueForProperty:MPMediaItemPropertyAssetURL]];
     
-    //reader and writer initialize
+    // reader and writer initialize
     NSError *error = nil;
     AVAssetReader *assetReader = [AVAssetReader assetReaderWithAsset:asset error:&error];
     if (!assetReader||error) {
@@ -87,7 +101,7 @@
         NSLog(@"assetReaderOutput failed: %@",error);
         return NO;
     }
-    //setting writer's output options
+    // setting writer's output options
     AudioChannelLayout audioChannelLayout;
 	memset(&audioChannelLayout, 0, sizeof(AudioChannelLayout));
 	audioChannelLayout.mChannelLayoutTag = kAudioChannelLayoutTag_Stereo;
@@ -128,16 +142,16 @@
         NSLog(@"assetWriter start writing has failed: %@",assetWriter.error);
         return NO;
     }
-    //set and ready to read and write to raw
+    // set and ready to read and write to raw
     AVAssetTrack *soundTrack = [asset.tracks objectAtIndex:0];
 	[assetWriter startSessionAtSourceTime:CMTimeMake(0, soundTrack.naturalTimeScale)];
     dispatch_queue_t mediaOutputQueue = dispatch_queue_create("mediaOutputQueue", NULL);
     [assetWriterInput requestMediaDataWhenReadyOnQueue:mediaOutputQueue usingBlock:^{
-        //start writing
+        // start writing
         while (assetWriterInput.readyForMoreMediaData) {
             CMSampleBufferRef bufferRef = [assetReaderOutput copyNextSampleBuffer];
             if (!bufferRef) {
-                //reader eof
+                // reader eof
                 [assetWriterInput markAsFinished];
 				[assetWriter finishWritingWithCompletionHandler:^{}];
 				[assetReader cancelReading];
@@ -152,7 +166,7 @@
                                                                   otherButtonTitles:nil];
                     [completeAlert show];
 				});
-                //write complete
+                // write complete
                 break;
             }
             [assetWriterInput appendSampleBuffer:bufferRef];
@@ -164,7 +178,7 @@
 
 - (NSURL *)exportURL
 {
-    //return the user document directory: iTunes file sharing
+    // return the user document directory: iTunes file sharing
 	NSString *docDirPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
 	NSString *exportFilePath = [docDirPath stringByAppendingPathComponent:@"RAW-exported.caf"];
     
@@ -176,6 +190,115 @@
 	}
     
 	return [NSURL fileURLWithPath:exportFilePath];
+}
+
+#pragma mark -
+#pragma mark AVPlayer functions
+
+// observing key path changes:
+// streamPlayer's playing rate (play/pause)
+// update stream buffered length and playback current time
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+    if (context == (__bridge void *)(streamPlayerPlaybackNotification)) {
+        if (_streamPlayer.rate==0.0) { // paused
+            [_playbackControl setTitle:@"Play" forState:UIControlStateNormal];
+        } else { // playing
+            [_playbackControl setTitle:@"Pause" forState:UIControlStateNormal];
+        }
+    }
+    if (context == (__bridge void *)(streamPlayerStatusNotification)) {
+        if (_streamPlayer.status==AVPlayerStatusReadyToPlay) {
+            [_streamPlayer play];
+            
+            if (_checkBufferedTimer) {
+                [_checkBufferedTimer invalidate];
+                _checkBufferedTimer = nil;
+            }
+            [_bufferBar setProgress:0.0];
+            _checkBufferedTimer = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(availableDuration) userInfo:nil repeats:YES];
+            [_durationLabel setText:[NSString stringWithFormat:@"%02d:%02d",(int)floor(CMTimeGetSeconds([[_streamPlayer currentItem] duration])/60),((int)floor(CMTimeGetSeconds([[_streamPlayer currentItem] duration])))%60]];
+            
+            if (_playbackTimeUpdate) {
+                [_playbackTimeUpdate invalidate];
+                _playbackTimeUpdate = nil;
+            }
+            [_playbackProgress setValue:0.0];
+            _playbackTimeUpdate = [NSTimer scheduledTimerWithTimeInterval:0.5 target:self selector:@selector(updatePlaybackProgress) userInfo:nil repeats:YES];
+            
+            [_spinner stopAnimating];
+        }
+    }
+}
+
+// stream select handler
+- (IBAction)streamSeleted:(UISegmentedControl *)sender forEvent:(UIEvent *)event {
+    NSURL *streamURL;
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            streamURL = [NSURL URLWithString:@"http://www.bryantung.info/audio/huntersmoon.mp3"];
+            break;
+            
+        case 1:
+            streamURL = [NSURL URLWithString:@"http://www.bryantung.info/audio/drifting.mp3"];
+            break;
+    }
+    // remove old player instance and start new player
+    [_streamPlayer removeObserver:self forKeyPath:@"rate" context:(__bridge void *)(streamPlayerPlaybackNotification)];
+    [_streamPlayer removeObserver:self forKeyPath:@"status" context:(__bridge void *)(streamPlayerStatusNotification)];
+    _streamPlayer = nil;
+    
+    AVPlayer *newPlayer = [[AVPlayer alloc] initWithURL:streamURL];
+    if (newPlayer.status==AVPlayerItemStatusFailed) {
+        NSLog(@"stream load failed");
+        [_durationLabel setText:@"--:--"];
+        [_timingLabel setText:@"--:--"];
+        return;
+    }
+    [newPlayer setActionAtItemEnd:AVPlayerActionAtItemEndPause];
+    [newPlayer addObserver:self forKeyPath:@"rate" options:0 context:(__bridge void *)(streamPlayerPlaybackNotification)];
+    _streamPlayer = newPlayer;
+    
+    [_spinner startAnimating];
+    [_streamPlayer addObserver:self forKeyPath:@"status" options:0 context:(__bridge void *)(streamPlayerStatusNotification)];
+}
+
+- (IBAction)playbackTap:(UIButton *)sender {
+    if ([[sender titleForState:UIControlStateNormal] isEqualToString:@"Play"]) {
+        // if player has reached the end
+        if (CMTimeGetSeconds(_streamPlayer.currentTime)>=CMTimeGetSeconds([_streamPlayer.currentItem duration])) {
+            [_streamPlayer seekToTime:kCMTimeZero];
+        }
+        [_streamPlayer play];
+        [sender setTitle:@"Pause" forState:UIControlStateNormal];
+    } else if ([[sender titleForState:UIControlStateNormal] isEqualToString:@"Pause"]) {
+        [_streamPlayer pause];
+        [sender setTitle:@"Play" forState:UIControlStateNormal];
+    }
+}
+// calculate stream buffer current length/progress
+- (NSTimeInterval)availableDuration
+{
+    NSArray *loadedTimeRanges = [[_streamPlayer currentItem] loadedTimeRanges];
+    CMTimeRange timeRange = [[loadedTimeRanges lastObject] CMTimeRangeValue];
+    float startSeconds = CMTimeGetSeconds(timeRange.start);
+    float durationSeconds = CMTimeGetSeconds(timeRange.duration);
+    NSTimeInterval totalDuration = CMTimeGetSeconds([[_streamPlayer currentItem] duration]);
+    NSTimeInterval result = startSeconds + durationSeconds;
+    [_bufferBar setProgress:(result/totalDuration)];
+    if (result>=totalDuration) {
+        [_checkBufferedTimer invalidate];
+        _checkBufferedTimer = nil;
+    }
+    return result;
+}
+
+- (void)updatePlaybackProgress
+{
+    NSTimeInterval currentTime = CMTimeGetSeconds([[_streamPlayer currentItem] currentTime]);
+    NSTimeInterval duration = CMTimeGetSeconds([[_streamPlayer currentItem] duration]);
+    [_playbackProgress setValue:(currentTime/duration)];
+    [_timingLabel setText:[NSString stringWithFormat:@"%02d:%02d",(int)floor(currentTime/60),((int)floor(currentTime))%60]];
 }
 
 #pragma mark -
